cf640b87d23130927c92c93b2c3becd27c4b5285
fivge <luanxingtong@gmail.com>
2019-11-25 16:19:52 +0800

    chore: add serviceWorker support

--- web/fibonacci.js
+++ web/fibonacci.js
@@ -1,3 +1,8 @@
+// service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/web/sw.js", { scope: "/web/" });
+}
+
 /**
  * 斐波那契数列，又称黄金分割数列，指的是这样一个数列：0、1、1、2、3、5、8、13、21、34、……
  * 在数学上，斐波纳契数列以如下被以递归的方法定义：F（0）=0，F（1）=1，F（n）=F(n-1)+F(n-2)（n≥2，n∈N*）
@@ -22,7 +27,9 @@ const runWasm = async () => {
   const resultJs = Fibonacci(value);
   console.timeEnd("runsJs");
 
-  console.log(`result:`, result, resultJs);
+  console.log(`result:`, value, result, resultJs);
+
+  // window.location.reload();
 };
 
 runWasm();
